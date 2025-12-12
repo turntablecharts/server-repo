e7af1b05faaa411c3daab89cb11fdbe5aaec363b
Mr. Simi <Mr. Simi>
2025-12-12 16:44:56 +0100

    order magazines by latests

--- Presentation/Controllers/MagazineController.cs
+++ Presentation/Controllers/MagazineController.cs
@@ -18,16 +18,18 @@ namespace Presentation.Controllers
     [Route("api/author")]
     public class MagazineController : ControllerBase
     {
-
         private IGenericRepository<TtcUser> _userGenericRepo;
         private readonly IGenericRepository<Log> _logRepository;
         private IGenericRepository<MagazineData> _magazineRepository;
 
         private IGenericRepository<MagazineEditionData> _magEditionRepository;
-        public MagazineController(IGenericRepository<TtcUser> userGenericRepo,
+
+        public MagazineController(
+            IGenericRepository<TtcUser> userGenericRepo,
             IGenericRepository<Log> logRepository,
             IGenericRepository<MagazineEditionData> magEditionRepository,
-            IGenericRepository<MagazineData> magazineRepository)
+            IGenericRepository<MagazineData> magazineRepository
+        )
         {
             _userGenericRepo = userGenericRepo;
             _logRepository = logRepository;
@@ -46,7 +48,9 @@ namespace Presentation.Controllers
             }
             item.DateCreated = DateTime.Now;
 
-            var user = _userGenericRepo.GetWithInclude(m => m.Email == item.Email, string.Empty).FirstOrDefault();
+            var user = _userGenericRepo
+                .GetWithInclude(m => m.Email == item.Email, string.Empty)
+                .FirstOrDefault();
 
             item.TtcUserId = user.Id;
 
@@ -61,7 +65,9 @@ namespace Presentation.Controllers
         {
             int pageSize = 20;
             var magazines = _magazineRepository.GetAll().OrderByDescending(m => m.DateCreated);
-            return Ok(await PaginatedList<MagazineData>.CreateAsync(magazines, pageNumber ?? 1, pageSize));
+            return Ok(
+                await PaginatedList<MagazineData>.CreateAsync(magazines, pageNumber ?? 1, pageSize)
+            );
         }
 
         [AllowAnonymous]
@@ -76,23 +82,24 @@ namespace Presentation.Controllers
 
             var result = new MagazineContentDto
             {
-                Id = magazine.Id, 
-                DateCreated = magazine.DateCreated, 
-                TtcUser = magazine.TtcUser, 
-                TtcUserId = magazine.TtcUserId, 
-                Title = magazine.Title, 
-                Description = magazine.Description, 
-                Content = magazine.Content, 
-                HeaderImage = magazine.HeaderImage, 
-                MagazineEditionDataId = magazine.MagazineEditionDataId, 
-                ArticlePosition = magazine.ArticlePosition, 
-                NextArticle = new MagazineData()
+                Id = magazine.Id,
+                DateCreated = magazine.DateCreated,
+                TtcUser = magazine.TtcUser,
+                TtcUserId = magazine.TtcUserId,
+                Title = magazine.Title,
+                Description = magazine.Description,
+                Content = magazine.Content,
+                HeaderImage = magazine.HeaderImage,
+                MagazineEditionDataId = magazine.MagazineEditionDataId,
+                ArticlePosition = magazine.ArticlePosition,
+                NextArticle = new MagazineData(),
             };
 
-            var magazines = _magEditionRepository.GetWithInclude(m => m.Id == magazine.MagazineEditionDataId, "MagazineDatas").FirstOrDefault();           
+            var magazines = _magEditionRepository
+                .GetWithInclude(m => m.Id == magazine.MagazineEditionDataId, "MagazineDatas")
+                .FirstOrDefault();
 
-           var outputOthers = magazines.MagazineDatas.OrderBy(m => m.ArticlePosition)
-                .ToList();
+            var outputOthers = magazines.MagazineDatas.OrderBy(m => m.ArticlePosition).ToList();
 
             var currentIndex = outputOthers.FindIndex(m => m.Id == magazine.Id);
 
@@ -107,7 +114,7 @@ namespace Presentation.Controllers
             //     {
             //         result.NextArticle = outputOthers[0];
             //     }
-            //     else 
+            //     else
             //     {
             //         result.NextArticle = outputOthers[currentIndex+1];
             //     }
@@ -118,9 +125,12 @@ namespace Presentation.Controllers
             return Ok(result);
         }
 
-
         [HttpPut("magazine/edit/{id}/{userEmail}")]
-        public async Task<IActionResult> EditMagazine([FromRoute] int id, [FromBody] MagazineData item, [FromRoute] string userEmail)
+        public async Task<IActionResult> EditMagazine(
+            [FromRoute] int id,
+            [FromBody] MagazineData item,
+            [FromRoute] string userEmail
+        )
         {
             if (!ModelState.IsValid)
             {
@@ -133,12 +143,14 @@ namespace Presentation.Controllers
             {
                 return NotFound();
             }
-            await _logRepository.AddAsync(new Log
-            {
-                Name = userEmail,
-                EventDate = DateTime.Now,
-                Event = "Edited Magazine, title: " + editedMagazine.Title
-            });
+            await _logRepository.AddAsync(
+                new Log
+                {
+                    Name = userEmail,
+                    EventDate = DateTime.Now,
+                    Event = "Edited Magazine, title: " + editedMagazine.Title,
+                }
+            );
             return Ok(editedMagazine);
         }
 
@@ -147,15 +159,16 @@ namespace Presentation.Controllers
         {
             try
             {
-
                 _magazineRepository.Delete(_magazineRepository.GetById(id));
 
-                _logRepository.AddAsync(new Log
-                {
-                    Name = userEmail,
-                    Event = "Deleted magazine, Id: " + id,
-                    EventDate = DateTime.Now
-                });
+                _logRepository.AddAsync(
+                    new Log
+                    {
+                        Name = userEmail,
+                        Event = "Deleted magazine, Id: " + id,
+                        EventDate = DateTime.Now,
+                    }
+                );
                 return Ok("Successfully deleted");
             }
             catch (NullReferenceException)
@@ -169,18 +182,21 @@ namespace Presentation.Controllers
         public IActionResult GetMagazineByEdition([FromRoute] string editionName)
         {
             //var magazines = _magEditionRepository.GetWithInclude(m => m.Name.ToLower() == editionName.ToLower(), "MagazineDatas").FirstOrDefault();
-            var magazines = _magEditionRepository.GetWithInclude(m => m.Name.ToLower() == editionName.ToLower(), "MagazineDatas").FirstOrDefault();
+            var magazines = _magEditionRepository
+                .GetWithInclude(m => m.Name.ToLower() == editionName.ToLower(), "MagazineDatas")
+                .FirstOrDefault();
             MagazineEditionData output = new MagazineEditionData();
-            var magazineData = magazines.MagazineDatas.Select(m => new {
-                Id = m.Id, 
-                DateCreated = m.DateCreated,
-                Title = m.Title, 
-                Writer = m.Description, 
-                HeaderImage = m.HeaderImage, 
-                magazineEditionDataId = m.MagazineEditionDataId, 
-                articlePosition = m.ArticlePosition
-                //Description = Regex.Replace(m.Content.Substring(0, 255)+"..", @"[^0-9a-zA-Z:,.']+", " ")
-                
+            var magazineData = magazines
+                .MagazineDatas.Select(m => new
+                {
+                    Id = m.Id,
+                    DateCreated = m.DateCreated,
+                    Title = m.Title,
+                    Writer = m.Description,
+                    HeaderImage = m.HeaderImage,
+                    magazineEditionDataId = m.MagazineEditionDataId,
+                    articlePosition = m.ArticlePosition,
+                    //Description = Regex.Replace(m.Content.Substring(0, 255)+"..", @"[^0-9a-zA-Z:,.']+", " ")
                 })
                 .OrderBy(m => m.articlePosition)
                 .ToList();
@@ -189,7 +205,12 @@ namespace Presentation.Controllers
             // output.Name = magazines.Name;
             // output.MagazineDatas = magazineData;
 
-            var result = new {Id = magazines.Id, Name = magazines.Name, magazineData = magazineData};
+            var result = new
+            {
+                Id = magazines.Id,
+                Name = magazines.Name,
+                magazineData = magazineData,
+            };
 
             return Ok(result);
         }
@@ -198,7 +219,9 @@ namespace Presentation.Controllers
         [HttpGet("magazine/pages/{editionName}")]
         public IActionResult GetMagazinePages([FromRoute] string editionName)
         {
-            var magazines = _magEditionRepository.GetWithInclude(m => m.Name.ToLower() == editionName.ToLower(), "MagazineDatas").FirstOrDefault();
+            var magazines = _magEditionRepository
+                .GetWithInclude(m => m.Name.ToLower() == editionName.ToLower(), "MagazineDatas")
+                .FirstOrDefault();
             foreach (var item in magazines.MagazineDatas)
             {
                 item.Content = null;
@@ -208,9 +231,8 @@ namespace Presentation.Controllers
 
             output.Id = magazines.Id;
             output.Name = magazines.Name;
-            
-            output.MagazineDatas = magazines.MagazineDatas.OrderBy(m => m.ArticlePosition)
-                .ToList();
+
+            output.MagazineDatas = magazines.MagazineDatas.OrderBy(m => m.ArticlePosition).ToList();
 
             return Ok(output);
         }
@@ -219,10 +241,12 @@ namespace Presentation.Controllers
         [HttpGet("magazine/editions")]
         public IActionResult GetMagazineEditions()
         {
-            var editions = _magEditionRepository.GetAll().Where(m => m.IsDelete == false);
+            var editions = _magEditionRepository
+                .GetAll()
+                .Where(m => m.IsDelete == false)
+                .OrderByDescending(m => m.Id);
             return Ok(editions);
         }
         #endregion
-
     }
 }
